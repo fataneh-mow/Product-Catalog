8bbe91b545b274ea8a0f688dbea877200ef4ca44
fataneh-mow <fataneh.mow@gmail.com>
2026-05-05 17:28:31 +0430

    feat: landing page created and other fixes done

--- melova/src/app-routes/AppRouter.jsx
+++ melova/src/app-routes/AppRouter.jsx
@@ -1,21 +1,12 @@
 import ProtectedRoutes from "./ProtectedRoutes";
-import PublicRoutes from "./PublicRoutes";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import AppLayout from "../layout/AppLayout";
-import AuthLayout from "../layout/AuthLayout";
 
 
 function AppRoutes() {
   return (
     <Routes>
-
-      <Route element={<AuthLayout />}>
-        {PublicRoutes.map((route, index) => (
-            <Route key={index} path={route.path} element={route.element} />
-          ))}
-      </Route>
-
       <Route path="/" element={<AppLayout />}>
         {ProtectedRoutes.map((route, index) => (
           <Route key={index} path={route.path} element={route.element} />
--- melova/src/app-routes/ProtectedRoutes.jsx
+++ melova/src/app-routes/ProtectedRoutes.jsx
@@ -1,8 +1,13 @@
 import Cart from "../pages/Cart";
+import Home from "../pages/Home";
 import Products from "../pages/Products";
 import Settings from "../pages/Settings";
 
 const ProtectedRoutes = [
+  {
+    path: "/",
+    element: <Home />
+  },
   {
     path: '/products',
     element: <Products />,
